b52f9c8548fe0e550d2c8b0f3638690b4658b6be
Toluwani Ajibare <toluwaniajibare@gmail.com>
2022-12-10 14:33:43 +0100

    Solved task 7

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-  for row in matrix:
-      for i, c in enumerate(row):
+  for r in matrix:
+      for i, c in enumerate(r):
           x = ' '
-          if i == len(row) - 1:
+          if i == len(r) - 1:
               x = ''
       print("{:d}".format(c, x), end=x)
     print("")
